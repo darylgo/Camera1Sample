2a747435712b9e17f216ddeb6c076ab09f048280
黄进达 <hjd@meitu.com>
2018-11-10 15:16:40 +0800

    增加预览回调的注册。

--- app/src/main/java/com/darylgo/camera/sample/MainActivity.java
+++ app/src/main/java/com/darylgo/camera/sample/MainActivity.java
@@ -1,7 +1,10 @@
 package com.darylgo.camera.sample;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.pm.PackageManager;
+import android.graphics.ImageFormat;
+import android.graphics.PixelFormat;
 import android.hardware.Camera;
 import android.os.Build;
 import android.os.Bundle;
@@ -13,6 +16,7 @@ import android.support.annotation.WorkerThread;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.OrientationEventListener;
 import android.view.Surface;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
@@ -31,10 +35,13 @@ public class MainActivity extends AppCompatActivity implements Handler.Callback 
     private static final int MSG_SET_PREVIEW_SURFACE = 4;
     private static final int MSG_START_PREVIEW = 5;
     private static final int MSG_STOP_PREVIEW = 6;
+    private static final int MSG_SET_PICTURE_SIZE = 7;
+    private static final int MSG_TAKE_PICTURE = 8;
 
     private static final String TAG = "MainActivity";
     private static final int REQUEST_PERMISSIONS_CODE = 1;
     private static final String[] REQUIRED_PERMISSIONS = {Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE};
+    private static final int PREVIEW_FORMAT = ImageFormat.NV21;
 
     @Nullable
     private HandlerThread mCameraThread = null;
@@ -60,30 +67,48 @@ public class MainActivity extends AppCompatActivity implements Handler.Callback 
     private int mPreviewSurfaceWidth;
     private int mPreviewSurfaceHeight;
 
+    @Nullable
+    private DeviceOrientationListener mDeviceOrientationListener;
+
     @Override
     public boolean handleMessage(Message msg) {
         switch (msg.what) {
-            case MSG_OPEN_CAMERA:
+            case MSG_OPEN_CAMERA: {
                 openCamera(msg.arg1);
                 break;
-            case MSG_CLOSE_CAMERA:
+            }
+            case MSG_CLOSE_CAMERA: {
                 closeCamera();
                 break;
-            case MSG_SET_PREVIEW_SIZE:
+            }
+            case MSG_SET_PREVIEW_SIZE: {
                 int shortSide = msg.arg1;
                 int longSide = msg.arg2;
                 setPreviewSize(shortSide, longSide);
                 break;
-            case MSG_SET_PREVIEW_SURFACE:
+            }
+            case MSG_SET_PREVIEW_SURFACE: {
                 SurfaceHolder previewSurface = (SurfaceHolder) msg.obj;
                 setPreviewSurface(previewSurface);
                 break;
-            case MSG_START_PREVIEW:
+            }
+            case MSG_START_PREVIEW: {
                 startPreview();
                 break;
-            case MSG_STOP_PREVIEW:
+            }
+            case MSG_STOP_PREVIEW: {
                 stopPreview();
                 break;
+            }
+            case MSG_SET_PICTURE_SIZE: {
+                int shortSide = msg.arg1;
+                int longSide = msg.arg2;
+                setPictureSize(shortSide, longSide);
+                break;
+            }
+            case MSG_TAKE_PICTURE: {
+                takePicture();
+            }
             default:
                 throw new IllegalArgumentException("Illegal message: " + msg.what);
         }
@@ -94,7 +119,11 @@ public class MainActivity extends AppCompatActivity implements Handler.Callback 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mDeviceOrientationListener = new DeviceOrientationListener(this);
+
         startCameraThread();
+
         initCameraInfo();
 
         SurfaceView cameraPreview = findViewById(R.id.camera_preview);
@@ -102,12 +131,18 @@ public class MainActivity extends AppCompatActivity implements Handler.Callback 
 
         Button switchCameraButton = findViewById(R.id.switch_camera);
         switchCameraButton.setOnClickListener(new OnSwitchCameraButtonClickListener());
+
+        Button takePictureButton = findViewById(R.id.take_picture);
+        takePictureButton.setOnClickListener(new OnTakePictureButtonClickListener());
     }
 
     @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        stopCameraThread();
+    protected void onStart() {
+        super.onStart();
+        DeviceOrientationListener deviceOrientationListener = mDeviceOrientationListener;
+        if (deviceOrientationListener != null) {
+            deviceOrientationListener.enable();
+        }
     }
 
     @Override
@@ -119,6 +154,30 @@ public class MainActivity extends AppCompatActivity implements Handler.Callback 
         } else if (mCameraHandler != null) {
             mCameraHandler.obtainMessage(MSG_OPEN_CAMERA, getCameraId(), 0).sendToTarget();
         }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (mCameraHandler != null) {
+            mCameraHandler.removeMessages(MSG_OPEN_CAMERA);
+            mCameraHandler.sendEmptyMessage(MSG_CLOSE_CAMERA);
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        DeviceOrientationListener deviceOrientationListener = mDeviceOrientationListener;
+        if (deviceOrientationListener != null) {
+            deviceOrientationListener.disable();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        stopCameraThread();
     }
 
     /**
@@ -144,15 +203,6 @@ public class MainActivity extends AppCompatActivity implements Handler.Callback 
             return mFrontCameraId;
         } else {
             throw new RuntimeException("No available camera id to switch.");
-        }
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        if (mCameraHandler != null) {
-            mCameraHandler.removeMessages(MSG_OPEN_CAMERA);
-            mCameraHandler.sendEmptyMessage(MSG_CLOSE_CAMERA);
         }
     }
 
@@ -298,8 +348,55 @@ public class MainActivity extends AppCompatActivity implements Handler.Callback 
             for (Camera.Size previewSize : supportedPreviewSizes) {
                 if ((float) previewSize.width / previewSize.height == aspectRatio && previewSize.height <= shortSide && previewSize.width <= longSide) {
                     parameters.setPreviewSize(previewSize.width, previewSize.height);
-                    camera.setParameters(parameters);
                     Log.d(TAG, "setPreviewSize() called with: width = " + previewSize.width + "; height = " + previewSize.height);
+
+                    if (isPreviewFormatSupported(parameters, PREVIEW_FORMAT)) {
+                        parameters.setPreviewFormat(PREVIEW_FORMAT);
+                        int frameWidth = previewSize.width;
+                        int frameHeight = previewSize.height;
+                        int previewFormat = parameters.getPreviewFormat();
+                        PixelFormat pixelFormat = new PixelFormat();
+                        PixelFormat.getPixelFormatInfo(previewFormat, pixelFormat);
+                        int bufferSize = (frameWidth * frameHeight * pixelFormat.bitsPerPixel) / 8;
+                        camera.addCallbackBuffer(new byte[bufferSize]);
+                        camera.addCallbackBuffer(new byte[bufferSize]);
+                        camera.addCallbackBuffer(new byte[bufferSize]);
+                        Log.d(TAG, "Add three callback buffers with size: " + bufferSize);
+                    }
+
+                    camera.setParameters(parameters);
+                    break;
+                }
+            }
+        }
+    }
+
+    /**
+     * 判断指定的预览格式是否支持。
+     */
+    private boolean isPreviewFormatSupported(Camera.Parameters parameters, int format) {
+        List<Integer> supportedPreviewFormats = parameters.getSupportedPreviewFormats();
+        return supportedPreviewFormats != null && supportedPreviewFormats.contains(format);
+    }
+
+    /**
+     * 根据指定的尺寸要求设置照片尺寸，我们会考虑指定尺寸的比例，并且去符合比例的最大尺寸作为照片尺寸。
+     *
+     * @param shortSide 短边长度
+     * @param longSide  长边长度
+     */
+    @WorkerThread
+    private void setPictureSize(int shortSide, int longSide) {
+        Camera camera = mCamera;
+        if (camera != null && shortSide != 0 && longSide != 0) {
+            float aspectRatio = (float) longSide / shortSide;
+            Camera.Parameters parameters = camera.getParameters();
+            List<Camera.Size> supportedPictureSizes = parameters.getSupportedPictureSizes();
+            for (Camera.Size pictureSize : supportedPictureSizes) {
+                if ((float) pictureSize.width / pictureSize.height == aspectRatio) {
+                    parameters.setPictureSize(pictureSize.width, pictureSize.height);
+                    camera.setParameters(parameters);
+                    Log.d(TAG, "setPictureSize() called with: width = " + pictureSize.width + "; height = " + pictureSize.height);
                     break;
                 }
             }
@@ -330,8 +427,22 @@ public class MainActivity extends AppCompatActivity implements Handler.Callback 
         Camera camera = mCamera;
         SurfaceHolder previewSurface = mPreviewSurface;
         if (camera != null && previewSurface != null) {
+            camera.setPreviewCallbackWithBuffer(new PreviewCallback());
             camera.startPreview();
             Log.d(TAG, "startPreview() called");
+        }
+    }
+
+    /**
+     * 拍照。
+     */
+    @WorkerThread
+    private void takePicture() {
+        Camera camera = mCamera;
+        if (camera != null) {
+            Camera.Parameters parameters = camera.getParameters();
+            camera.setParameters(parameters);
+            camera.takePicture(new ShutterCallback(), new RawCallback(), new PostviewCallback(), new JpegCallback());
         }
     }
 
@@ -365,15 +476,6 @@ public class MainActivity extends AppCompatActivity implements Handler.Callback 
         return mFrontCameraId != -1;
     }
 
-    private void setupPreview(SurfaceHolder previewSurface, int surfaceWidth, int surfaceHeight) {
-        Handler cameraHandler = mCameraHandler;
-        if (cameraHandler != null) {
-            cameraHandler.obtainMessage(MSG_SET_PREVIEW_SIZE, surfaceWidth, surfaceHeight).sendToTarget();
-            cameraHandler.obtainMessage(MSG_SET_PREVIEW_SURFACE, previewSurface).sendToTarget();
-            cameraHandler.sendEmptyMessage(MSG_START_PREVIEW);
-        }
-    }
-
     private class OnSwitchCameraButtonClickListener implements View.OnClickListener {
         @Override
         public void onClick(View v) {
@@ -387,9 +489,65 @@ public class MainActivity extends AppCompatActivity implements Handler.Callback 
                 cameraHandler.sendEmptyMessage(MSG_CLOSE_CAMERA);// 关闭当前的摄像头
                 cameraHandler.obtainMessage(MSG_OPEN_CAMERA, cameraId, 0).sendToTarget();// 开启新的摄像头
                 cameraHandler.obtainMessage(MSG_SET_PREVIEW_SIZE, previewSurfaceWidth, previewSurfaceHeight).sendToTarget();// 配置预览尺寸
+                cameraHandler.obtainMessage(MSG_SET_PICTURE_SIZE, previewSurfaceWidth, previewSurfaceHeight).sendToTarget();// 配置照片尺寸
                 cameraHandler.obtainMessage(MSG_SET_PREVIEW_SURFACE, previewSurface).sendToTarget();// 配置预览 Surface
                 cameraHandler.sendEmptyMessage(MSG_START_PREVIEW);// 开启预览
             }
+        }
+    }
+
+    private class PreviewCallback implements Camera.PreviewCallback {
+        @Override
+        public void onPreviewFrame(byte[] data, Camera camera) {
+            // 在使用完 Buffer 之后记得回收复用。
+            camera.addCallbackBuffer(data);
+        }
+    }
+
+    private class OnTakePictureButtonClickListener implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            takePicture();
+        }
+    }
+
+    private class DeviceOrientationListener extends OrientationEventListener {
+
+        private DeviceOrientationListener(Context context) {
+            super(context);
+        }
+
+        @Override
+        public void onOrientationChanged(int orientation) {
+
+        }
+    }
+
+    private class ShutterCallback implements Camera.ShutterCallback {
+        @Override
+        public void onShutter() {
+            Log.d(TAG, "onShutter() called");
+        }
+    }
+
+    private class RawCallback implements Camera.PictureCallback {
+        @Override
+        public void onPictureTaken(byte[] data, Camera camera) {
+            Log.d(TAG, "On raw taken.");
+        }
+    }
+
+    private class PostviewCallback implements Camera.PictureCallback {
+        @Override
+        public void onPictureTaken(byte[] data, Camera camera) {
+            Log.d(TAG, "On postview taken.");
+        }
+    }
+
+    private class JpegCallback implements Camera.PictureCallback {
+        @Override
+        public void onPictureTaken(byte[] data, Camera camera) {
+            Log.d(TAG, "On jpeg taken.");
         }
     }
 
@@ -404,7 +562,13 @@ public class MainActivity extends AppCompatActivity implements Handler.Callback 
             mPreviewSurface = holder;
             mPreviewSurfaceWidth = width;
             mPreviewSurfaceHeight = height;
-            setupPreview(holder, width, height);
+            Handler cameraHandler = mCameraHandler;
+            if (cameraHandler != null) {
+                cameraHandler.obtainMessage(MSG_SET_PREVIEW_SIZE, width, height).sendToTarget();
+                cameraHandler.obtainMessage(MSG_SET_PICTURE_SIZE, width, height).sendToTarget();
+                cameraHandler.obtainMessage(MSG_SET_PREVIEW_SURFACE, holder).sendToTarget();
+                cameraHandler.sendEmptyMessage(MSG_START_PREVIEW);
+            }
         }
 
         @Override
